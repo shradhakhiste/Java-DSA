c1e6d18bed00b01168605f128bdb61fcd4bab1d7
ShrdhaKhiste <shradhakhiste@gmail.com>
2026-09-22 09:58:12 +0530

    Update array problems

--- src/Arrayproblem/SecLarge.java
+++ src/Arrayproblem/SecLarge.java
@@ -2,30 +2,24 @@ package Arrayproblem;
 
 public class SecLarge {
     public static void main(String[] args) {
+        int []arr={10,5,20,8,15};
+        int max1=arr[0];
+        int max2=arr[1];
 
-        int[] arr = {10, 5, 8, 20, 15};
-
-        int max1 = arr[0];
-        int max2 = arr[1];
-
-        if (max1 < max2) {
-            int temp = max1;
-            max1 = max2;
-            max2 = temp;
-        }
-
-        for (int i = 2; i < arr.length; i++) {
-
-            if (arr[i] > max1) {
-                max2 = max1;
-                max1 = arr[i];
-
-            } else if (arr[i] > max2) {
-                max2 = arr[i];
+        for(int i=2;i<arr.length;i++){
+            if(arr[i]>max1){
+                int temp=max1;
+                max1=arr[i];
+                max2=temp;
+            }else if( arr[i]>max2){
+                max2=arr[i];
             }
-        }
 
-        System.out.println("Largest = " + max1);
-        System.out.println("Second Largest = " + max2);
-    }
+        }
+        System.out.println(max1);
+        System.out.println(max2);
+
+
+
+}
 }
